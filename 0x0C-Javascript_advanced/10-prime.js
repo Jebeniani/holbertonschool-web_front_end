b5ad69d33eb14cecc68dc44a8f31adb417aa6fed
jebeniani <aymenjebeniani05@gmail.com>
2023-01-22 13:01:49 +0100

    Execution stack & timing execution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,24 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.time("Execution time of printing countPrimeNumbers 100 times");
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+}
+const result = countPrimeNumbers();
+console.timeEnd("Execution time of printing countPrimeNumbers 100 times");
+console.log(`Execution time of printing countPrimeNumbers 100 times was ${result} milliseconds.`);
